cdbc8ccabe46760ab1732434c5760875e189b7b0
pradip17sht <pradip17sht@gmail.com>
2020-09-16 14:41:24 +0545

    change on model

---
+++ SalesTransaction.Application.Model/Account/MvAccount.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesTransaction.Application.Model
+{
+    public class MvAccount
+    {
+    }
+    public class MvLogin { 
+        public int UserID { get; set; }
+        [DisplayName("UserName")]
+        public string UserName { get; set; }
+        [DisplayName("Password")]
+        public string Password { get; set; }
+
+    }
+}
