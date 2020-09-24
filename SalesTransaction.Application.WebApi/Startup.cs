992577e783d8399edc7411aa9617e227ea285462
pradip17sht <pradip17sht@gmail.com>
2020-09-24 18:09:59 +0545

    product service added in startup

--- SalesTransaction.Application.WebApi/Startup.cs
+++ SalesTransaction.Application.WebApi/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using SalesTransaction.Application.Service.Account;
+using SalesTransaction.Application.Service.Customer;
 using SalesTransaction.Application.Service.Product;
 
 namespace SalesTransaction.Application.WebApi
@@ -41,6 +42,8 @@ namespace SalesTransaction.Application.WebApi
             services.AddTransient<IAccountService, AccountService>();
 
             services.AddTransient<IProductService, ProductService>();
+
+            services.AddTransient<ICustomerService, CustomerService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
