33173caca1e1473977cd15e5aa7d87c6cb976515
pradip17sht <pradip17sht@gmail.com>
2020-09-24 10:46:51 +0545

    login error not solved

---
+++ SalesTransaction.Application.Model/Product/MvProduct.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace SalesTransaction.Application.Model.Product
+{
+    class MvProduct
+    {
+        [Required]
+        public string ProductName { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public int Price { get; set; }
+        [Required]
+        public int Rate { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        [Required]
+        public int InsertPersonId { get; set; }
+    }
+}
