1fa93ce4de10780cdfd59be3ef3b12b3b65669a6
pradip17sht <pradip17sht@gmail.com>
2020-09-24 18:06:54 +0545

    account model as userid added

--- SalesTransaction.Application.Model/Account/MvAccount.cs
+++ SalesTransaction.Application.Model/Account/MvAccount.cs
@@ -9,6 +9,8 @@ namespace SalesTransaction.Application.Model
     {
     }
     public class MvLogin { 
+
+        public int UserId { get; set; }
         [Required]
         public string UserName { get; set; }
         [Required]
