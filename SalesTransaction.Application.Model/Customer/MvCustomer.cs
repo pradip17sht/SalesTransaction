e4d8710601685462714ca5d1a5dc6e717cbb8f94
pradip17sht <pradip17sht@gmail.com>
2020-09-26 15:51:54 +0545

    customer add done in backend

--- SalesTransaction.Application.Model/Customer/MvCustomer.cs
+++ SalesTransaction.Application.Model/Customer/MvCustomer.cs
@@ -5,7 +5,27 @@ using System.Text;
 
 namespace SalesTransaction.Application.Model.Customer
 {
-    class MvCustomer
+    public class MvCustomer
+    {
+        public int customerId { get; set; }
+        [Required]
+        public string firstName { get; set; }
+        [Required]
+        public string middleName { get; set; }
+        [Required]
+        public string lastName { get; set; }
+        [Required]
+        public string customerAddress { get; set; }
+        [Required]
+        public string emailId { get; set; }
+        [Required]
+        public int phoneNo { get; set; }
+        [Required]
+        public string district { get; set; }
+        public int insertPersonId { get; set; }
+    }
+
+    public class MvEditCustomer
     {
         public int CustomerId { get; set; }
         [Required]
